e7e9f750ff283461255c544301acdafba57bff97
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-04 16:12:22 -0300

    first

---
+++ main.js
@@ -0,0 +1,59 @@
+//Simulação de DB
+const login = {
+    email: 'xx@xx.com',
+    senha: 123,
+}
+
+const empresas = [
+    {
+        nome: 'Empresa X',
+        cnpj: 123,
+        cidade: 'ecopos',
+        coordenadas: 0000,
+        informacoesGerais: 'Telefone: 226299561, encarregado Antonio'
+    },
+    {
+        nome: 'Empresa A',
+        cnpj: 1231,
+        cidade: 'ecopos',
+        coordenadas: 0000,
+        informacoesGerais: 'Telefone: 226299561, encarregado Antonio'
+    },
+    {
+        nome: 'Empresa D',
+        cnpj: 12312,
+        cidade: 'xico',
+        coordenadas: 0000,
+        informacoesGerais: 'Telefone: 226299561, encarregado Antonio'
+    },
+    {
+        nome: 'Empresa T',
+        cnpj: 123123,
+        cidade: 'ecopos',
+        coordenadas: 0000,
+        informacoesGerais: 'Telefone: 226299561, encarregado Antonio'
+    },
+    {
+        nome: 'Empresa O',
+        cnpj: 1230,
+        cidade: 'xico',
+        coordenadas: 0000,
+        informacoesGerais: 'Telefone: 226299561, encarregado Antonio'
+    }
+]
+
+/*********************************************************************************/
+
+// const btnAcessar = document.getElementById('btnAcessar');
+
+
+// btnAcessar.addEventListener('click', (e) => {
+//     const email = document.getElementById('email');
+//     const senha = document.getElementById('senha');
+//     e.preventDefault();
+//     window.location.replace("http://127.0.0.1:5500/infos.html");
+//     console.log(email.value + ' ' + senha.value)
+//     // if(login.senha === senha.value && login.email === email.value) {
+//     //     window.location.replace("http://127.0.0.1:5500/infos.html");
+//     // }
+// })
