41726ba85f614a09a5402b61d776b4cc8115a19d
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-05 09:21:23 -0300

    update busca

--- main.js
+++ main.js
@@ -47,17 +47,47 @@ const empresas = [
 
 
 const cpnjInput = document.getElementById('cnpjInput');
-let table = document.createElement('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
+const cidadeInput = document.getElementById('cidadeInbut');
+let table = document.getElementById('table');
 
-table.appendChild(thead);
-table.appendChild(tbody);
+table.innerHTML = `<thead><tr>
+<th>Nome da Empresa</th>
+<th>CNPJ da Empresa</th>
+<th>Cidade</th>
+<th>Informações gerais</th>
+<th>Coordenadas</th>
+</tr></thead>`
 
 function pesquisarPorCnpj() {    
     let cnpjEmpresa = empresas.filter((empresa) => {                
         return empresa.cnpj === Number(cpnjInput.value);
     })
 
-    console.log(cnpjEmpresa[0].nome);    
+    table.innerHTML += `        
+        <tbody><tr>
+            <td>${cnpjEmpresa[0].nome}</td>
+            <td>${cnpjEmpresa[0].cnpj}</td>
+            <td>${cnpjEmpresa[0].cidade}</td>
+            <td>${cnpjEmpresa[0].informacoesGerais}</td>
+            <td>${cnpjEmpresa[0].coordenadas}</td>
+        </tr></tbody>
+        ` 
+}
+
+function pesquisarPorCidade() {    
+    let cidadeEmpresa = empresas.filter((empresa) => {                
+        return empresa.cidade === cidadeInput.value;
+    })
+    
+    for(let i = 0; i < cidadeEmpresa.length; i++) {
+        table.innerHTML += `        
+        <tbody><tr>
+            <td>${cidadeEmpresa[i].nome}</td>
+            <td>${cidadeEmpresa[i].cnpj}</td>
+            <td>${cidadeEmpresa[i].cidade}</td>
+            <td>${cidadeEmpresa[i].informacoesGerais}</td>
+            <td>${cidadeEmpresa[i].coordenadas}</td>
+        </tr></tbody>
+        ` 
+    }    
 }
