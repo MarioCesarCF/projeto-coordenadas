445df3e32a091c0b1b73ece2194457b8e77283fe
Mário Cesar Cunha <mc.fialho@hotmail.com>
2022-08-16 07:47:24 -0300

    atualização da tabela na busca

--- main.js
+++ main.js
@@ -43,20 +43,27 @@ const empresas = [
 
 
 const cnpjInput = document.getElementById('cnpjInput');
-let cidadeInput = document.getElementById('cidadeInbut');
+const cidadeInput = document.getElementById('cidadeInbut');
+const nomeInput = document.getElementById('nomeInput');
 let table = document.getElementById('table');
-let thead = document.createElement('thead');
-let tbody = document.createElement('tbody');
-const btnBuscar = document.getElementById('btnBuscar');
+// let thead = document.createElement('thead');
+// let tbody = document.createElement('tbody');
+const btnBuscarCnpj = document.getElementById('btnBuscarCnpj');
+const btnBuscarCidade = document.getElementById('btnBuscarCidade');
+const btnBuscarNome = document.getElementById('btnBuscarNome');
 
-table.appendChild(thead);
-table.appendChild(tbody);
+// table.appendChild(thead);
+// table.appendChild(tbody);
 
-console.log(cnpjInput)
-
-btnBuscar.addEventListener('click', (e) => {
+btnBuscarCnpj.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(cnpjInput)
+  table.innerHTML = `<thead><tr>
+    <th>Nome da Empresa</th>
+    <th>CNPJ da Empresa</th>
+    <th>Cidade</th>
+    <th>Informações gerais</th>
+    <th>Coordenadas</th>
+    </tr></thead>`;
   let cnpjEmpresa = empresas.filter((empresa) => {
     return empresa.cnpj === Number(cnpjInput.value);
   })
@@ -73,6 +80,61 @@ btnBuscar.addEventListener('click', (e) => {
   cnpjInput.value = '';
 })
 
+btnBuscarCidade.addEventListener('click', (e) => {
+  e.preventDefault();
+  table.innerHTML = `<thead><tr>
+    <th>Nome da Empresa</th>
+    <th>CNPJ da Empresa</th>
+    <th>Cidade</th>
+    <th>Informações gerais</th>
+    <th>Coordenadas</th>
+    </tr></thead>`;
+  let cidadeEmpresa = empresas.filter((empresa) => {
+    return empresa.cidade === cidadeInput.value;
+  })
+
+  for (let i = 0; i < cidadeEmpresa.length; i++) {
+    table.innerHTML += `        
+      <tbody><tr>
+        <td>${cidadeEmpresa[i].nome}</td>
+        <td>${cidadeEmpresa[i].cnpj}</td>
+        <td>${cidadeEmpresa[i].cidade}</td>
+        <td>${cidadeEmpresa[i].informacoesGerais}</td>
+        <td>${cidadeEmpresa[i].coordenadas}</td>
+      </tr></tbody>
+    `
+  }
+  cidadeInput.value = '';
+})
+
+btnBuscarNome.addEventListener('click', (e) => {
+  e.preventDefault();
+  table.innerHTML = `<thead><tr>
+    <th>Nome da Empresa</th>
+    <th>CNPJ da Empresa</th>
+    <th>Cidade</th>
+    <th>Informações gerais</th>
+    <th>Coordenadas</th>
+    </tr></thead>`;
+  let nomeEmpresa = empresas.filter((empresa) => {
+    return empresa.nome === nomeInput.value;
+  })
+
+  for (let i = 0; i < nomeEmpresa.length; i++) {
+    table.innerHTML += `        
+      <tbody><tr>
+        <td>${nomeEmpresa[0].nome}</td>
+        <td>${nomeEmpresa[0].cnpj}</td>
+        <td>${nomeEmpresa[0].cidade}</td>
+        <td>${nomeEmpresa[0].informacoesGerais}</td>
+        <td>${nomeEmpresa[0].coordenadas}</td>
+      </tr></tbody>
+    `
+  }
+
+  nomeInput.value = '';
+})
+
 // function pesquisarPorCnpj() {
 //   table.innerHTML = `<thead><tr>
 //     <th>Nome da Empresa</th>
@@ -85,7 +147,7 @@ btnBuscar.addEventListener('click', (e) => {
 //     return empresa.cnpj === Number(cpnjInput.value);
 //   })
 
-//   table.innerHTML += `        
+//   table.innerHTML += `
 //         <tbody><tr>
 //             <td>${cnpjEmpresa[0].nome}</td>
 //             <td>${cnpjEmpresa[0].cnpj}</td>
@@ -111,17 +173,7 @@ btnBuscar.addEventListener('click', (e) => {
 //     return empresa.cidade === cidadeInput.value;
 //   })
 
-//   for (let i = 0; i < cidadeEmpresa.length; i++) {
-//     table.innerHTML += `        
-//         <tbody><tr>
-//             <td>${cidadeEmpresa[i].nome}</td>
-//             <td>${cidadeEmpresa[i].cnpj}</td>
-//             <td>${cidadeEmpresa[i].cidade}</td>
-//             <td>${cidadeEmpresa[i].informacoesGerais}</td>
-//             <td>${cidadeEmpresa[i].coordenadas}</td>
-//         </tr></tbody>
-//         `
-//   }
+//
 
 //   cidadeInput.value = '';
 // }
